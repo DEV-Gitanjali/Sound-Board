d17e654b59fb891854421a17bf268eb7f9aac3f5
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-01-07 11:08:18 +0530

    this code for how to stop & play sound board

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sounds=['applause','boo' , 'gasp','tada','victory','wrong']
 
+// this js for how to create button
 sounds.forEach(sound =>{
     const btn=document.createElement('button')
 
@@ -7,10 +8,21 @@ sounds.forEach(sound =>{
 
     btn.innerText=sound
 
-    btn.addEventListener('click',()=>{
-        stopsongs()
+    // how to create sound play & sound stop 
+    btn.addEventListener('click', () => {
+        stopSongs()
+
         document.getElementById(sound).play()
     })
 
     document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
